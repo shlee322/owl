298bdd2311745f9c3272973ecd2214257d2d8ba6
stephan@stephan-PC <stephan@stephan-PC>
2011-07-31 14:52:12 +0900

    test client for c# tested Dynamic data display dll test c#에서 클라이언트 테스트 완료 DDD dll 추가 및 xaml 에서 사용

---
+++ test/client_c#/client/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Research.DynamicDataDisplay;
+using Thrift;
+using Thrift.Collections;
+using Thrift.Protocol;
+using Thrift.Server;
+using Thrift.Transport;
+using contorlApi;
+
+namespace client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public static byte[] strToBytes(string str)
+        {
+            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            return encoding.GetBytes(str);
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            int timeout = 10 * 1000;
+            
+            TSocket socket = new TSocket("127.0.0.1",9099);
+            socket.Timeout=timeout;
+
+            TTransport transport = new TFramedTransport(socket);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            controlApi.Client client1 = new controlApi.Client(protocol);
+
+            transport.Open();
+
+
+            var mailset = new mailSet();
+            mailset.SenderAddress = "toori67@gmail.com";
+            mailset.Template = "<p>awdasc<p>";
+            mailset.WhenToSend = "2011.08.12";
+            mailset.AddressBook = strToBytes("this may be addressbook");
+
+
+            client1.sendMailSet(mailset);
+
+            textBox.Text = client1.getStatic("2011.08.12");
+        }
+    }
+}
