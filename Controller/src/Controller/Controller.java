93cd6cc56ef5345b35e2e779422da6285b7555ad
Ou <none@none>
2011-08-06 00:03:22 +0900

    망고DB 테스트... 일단 jar파일 추가하고.. 사이트 예제 긁어 왔는데.. 오류 ㅠㅠ 내일 아침에 만져봐야지 ㅠ

--- Controller/src/Controller/Controller.java
+++ Controller/src/Controller/Controller.java
@@ -2,6 +2,8 @@ package Controller;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Set;
 import java.util.concurrent.Executors;
 
 import org.apache.thrift.protocol.TBinaryProtocol;
@@ -12,24 +14,65 @@ import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TTransportException;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
+import com.mongodb.*;
+
 import com.googlecode.protobuf.netty.*;
-import com.googlecode.protobuf.netty.example.CalculatorServiceImpl;
+
 public class Controller {
 	static NettyRpcServer Sender_Server;
 	static TServer Client_Server;
 	public static void main(String ar[]) throws TTransportException
-	{
+	{/*
 		Controller.Sender_Server = new NettyRpcServer(new NioServerSocketChannelFactory(Executors.newCachedThreadPool(),Executors.newCachedThreadPool()));
 		
-		Controller.Sender_Server.registerService(Protocol.SenderController.SenderHandler.newReflectiveService(new CalculatorServiceImpl()));
+		Controller.Sender_Server.registerService(Protocol.SenderController.SenderHandler.newReflectiveService(new SenderHandler()));
 		Controller.Sender_Server.serve(new InetSocketAddress(7004));
 
 		final TNonblockingServerSocket socket = new TNonblockingServerSocket(9099);
 		final controlApi.controlApi.Processor processor = new controlApi.controlApi.Processor(new ClientHandler());
 		Controller.Client_Server = new THsHaServer(processor, socket, new TFramedTransport.Factory(), new TBinaryProtocol.Factory());
-		Controller.Client_Server.serve();
-		
+		Controller.Client_Server.serve();*/
+		Controller.MonggoDB();
 		
 		//일단은 대충대충 만듬
+	}
+	
+	public static void MonggoDB()
+	{
+		try{
+			Mongo m = new Mongo();
+
+			DB db = m.getDB( "mydb" );
+
+			Set<String> colls = db.getCollectionNames();
+
+			for (String s : colls) {
+			    System.out.println(s);
+			}
+			
+			DBCollection coll = db.getCollection("testCollection");
+
+
+			BasicDBObject doc = new BasicDBObject();
+
+	        doc.put("name", "MongoDB");
+	        doc.put("type", "database");
+	        doc.put("count", 1);
+
+	        BasicDBObject info = new BasicDBObject();
+
+	        info.put("x", 203);
+	        info.put("y", 102);
+
+	        doc.put("info", info);
+
+	        coll.insert(doc);
+
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+
 	}
 }
