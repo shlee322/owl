9dcdcb6857da1ee66a59b7076c834179d617c276
Ou <none@none>
2011-08-14 02:04:19 +0900

    몽고DB 주소록부분 90%완료? ㅋㅋ 내일 메일리스트부분 구현하자!

--- Controller/src/Controller/Controller.java
+++ Controller/src/Controller/Controller.java
@@ -3,6 +3,7 @@ package Controller;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Executors;
@@ -23,7 +24,7 @@ public class Controller {
 	static NettyRpcServer Sender_Server;
 	static TServer Client_Server;
 	public static void main(String ar[]) throws TTransportException
-	{
+	{/*
 		Controller.Sender_Server = new NettyRpcServer(new NioServerSocketChannelFactory(Executors.newCachedThreadPool(),Executors.newCachedThreadPool()));
 		
 		Controller.Sender_Server.registerService(Protocol.SenderController.SenderHandler.newReflectiveService(new SenderHandler()));
@@ -32,124 +33,148 @@ public class Controller {
 		final TNonblockingServerSocket socket = new TNonblockingServerSocket(9099);
 		final controlApi.controlApi.Processor processor = new controlApi.controlApi.Processor(new ClientHandler());
 		Controller.Client_Server = new THsHaServer(processor, socket, new TFramedTransport.Factory(), new TBinaryProtocol.Factory());
-		Controller.Client_Server.serve();
-		
-		Controller.MonggoDB();
+		Controller.Client_Server.serve();*/
 		
 		//일단은 대충대충 만듬
+		MongoDB MongoDB = new MongoDB();
+		MongoDB.DBStart("jin7h");
+		//MongoDB.Del_User("poweroyh");
+		MongoDB.Add_Group("SWmaestro");
+		MongoDB.Add_Person("SWmaestro", "오유환", "poweroyh@naver.com", "010-2563-7816");
+		MongoDB.Add_Person("SWmaestro", "이성민", "poweroyh@naver.com", "010-1111-2222");
+		MongoDB.Add_Person("SWmaestro", "이상혁", "poweroyh@naver.com", "010-3333-4444");
+		MongoDB.Add_Person("SWmaestro", "임성은", "ㅁㅁㄴㅇㅁㄴㅇ@naver.com", "010-2563-7816");
+		MongoDB.Add_Person("SWmaestro", "전수열", "poweroyh@naver.com", "1111-2222-7816");
+		MongoDB.Add_Person("SWmaestro", "김종헌", "poweroyh@naver.com", "010-2563-3333");
+		MongoDB.printResults();
+		MongoDB.Add_Group("SWmaestro");
+		
 	}
+}
+
+class MongoDB
+{
+	static Mongo m;
+	static DB db;
+	static String UserName;
+	static String GroupName;
+	static String SendMailList;
 	
-	public static void MonggoDB()
+	DBCollection GroupColl;
+	
+	void Del_User(String User)
+	{	
+		UserName = User;
+		m.dropDatabase(UserName);
+	}
+
+	//디비 시작
+	void DBStart(String User)
 	{
 		try{
-			Mongo m = new Mongo("controller.owl.or.kr");
-
-			DB db = m.getDB( "test" );
-
+			m = new Mongo("controller.owl.or.kr");
+			
+			UserName = User;
+			db = m.getDB(UserName);
+	
 			boolean auth = db.authenticate("owl","70210".toCharArray());
-			
-			m.dropDatabase("test");
-
-			Set<String> colls = db.getCollectionNames();
-
-			for (String s : colls) {
-			    System.out.println(s);
-			}
-
-			
-			DBCollection coll = db.getCollection("testCollection");
-
-			BasicDBObject doc = new BasicDBObject();
-
-	        doc.put("name", "MongoDB");
-	        doc.put("type", "database");
-	        doc.put("count", 1);
-
-	        BasicDBObject info = new BasicDBObject();
-
-	        info.put("x", 203);
-	        info.put("y", 102);
-
-	        doc.put("info", info);
-
-	        coll.insert(doc);
-	        
-	        DBObject myDoc = coll.findOne();
-	        System.out.println(myDoc);
-
-	        for (int i=0; i < 100; i++) {
-	            coll.insert(new BasicDBObject().append("i", i));
-	        }
-
-	        System.out.println(coll.getCount());
-
-	        DBCursor cur = coll.find();
-
-	        while(cur.hasNext()) {
-	            System.out.println(cur.next());
-	        }
-	        
-	        BasicDBObject query = new BasicDBObject();
-
-	        query.put("name", "MongoDB");
-
-	        cur = coll.find(query);
-
-	        while(cur.hasNext()) {
-	            System.out.println(cur.next());
-	        }
-	        
-	        BasicDBObject query2 = new BasicDBObject();
-
-	        query2.put("j", new BasicDBObject("$ne", 3));
-	        query2.put("k", new BasicDBObject("$gt", 10));
-
-	        cur = coll.find(query2);
-
-	        while(cur.hasNext()) {
-	            System.out.println(cur.next());
-	        }
-	        
-	        query = new BasicDBObject();
-
-	        query.put("i", new BasicDBObject("$gt", 50));  // e.g. find all where i > 50
-
-	        cur = coll.find(query);
-
-	        while(cur.hasNext()) {
-	            System.out.println(cur.next());
-	        }
-
-
-	        query = new BasicDBObject();
-
-	        query.put("i", new BasicDBObject("$gt", 20).append("$lte", 30));  // i.e.   20 < i <= 30
-
-	        cur = coll.find(query);
-
-	        while(cur.hasNext()) {
-	            System.out.println(cur.next());
-	        }
-	        
-	        coll.createIndex(new BasicDBObject("i", 1));  // create index on "i", ascending
-	        
-	        List<DBObject> list = coll.getIndexInfo();
-
-	        for (DBObject o : list) {
-	            System.out.println(o);
-	        }
-
-	        Mongo x = new Mongo();
-
-	        for (String s : x.getDatabaseNames()) {
-	            System.out.println(s);
-	        }
-
 		}
 		catch (Exception e) {
 			// TODO: handle exception
 		}
-		
-
 	}
+	
+	//그룹 추가
+	void Add_Group(String G_Name)
+	{
+		GroupName = UserName + "_" + G_Name;
+		
+		Set<String> colls = db.getCollectionNames();
+		
+		if(!colls.isEmpty())
+		{
+			if(db.collectionExists(GroupName))
+			{
+				System.out.println("같은 그룹명 있음");
+			}
+			else
+			{
+				GroupColl = db.createCollection(GroupName, null);
+			}
+		}
+		else
+		{
+			GroupColl = db.createCollection(GroupName, null);
+		}
+	}
+	
+	//그룹 리스트 로드
+	ArrayList<Person> Load_Group(String G_Name)
+	{
+		ArrayList<Person> PersonList = new ArrayList<Person>();
+		GroupName = UserName + "_" + G_Name;
+		
+		GroupColl = db.getCollection(GroupName);
+		
+		if(GroupColl.getCount() != 0)
+		{
+			DBCursor cur;
+			
+			cur = GroupColl.find();
+
+			while (cur.hasNext())
+			{
+				Person P_Temp = new Person();
+				P_Temp.Name = (String) cur.curr().get("name");
+				P_Temp.Mail_Address = (String) cur.curr().get("mail");
+				P_Temp.Phone = (String) cur.curr().get("phone");
+				
+				PersonList.add(P_Temp);
+			}		
+		}
+		else
+		{
+			System.out.println("그룹에 사람 없음");
+		}
+		return PersonList;
+	}
+	
+	//사람 추가
+	void Add_Person(String G_Name, String P_Name, String Mail_Address, String Phone)
+	{
+		GroupName = UserName + "_" + G_Name;
+		GroupColl = db.getCollection(GroupName);
+		int num = (int) GroupColl.getCount();
+		GroupColl.insert(MakePersonDocument(num+1, P_Name, Mail_Address, Phone));		
+	}
+	
+	void printResults()
+	{
+		DBCursor cur;
+		
+		cur = GroupColl.find();
+
+		while (cur.hasNext())
+		{
+			System.out.println(cur.next().get("index") + "," + cur.curr().get("name") + "," + cur.curr().get("mail") + "," + cur.curr().get("phone"));
+		}
+	}
+
+	private static BasicDBObject MakePersonDocument(int id, String P_Name, String Mail_Address, String Phone)
+	{
+		BasicDBObject doc = new BasicDBObject();
+		doc.put("id", id);
+		doc.put("name", P_Name);
+		doc.put("mail", Mail_Address);
+		doc.put("phone", Phone);
+		return doc;
+	}
+}
+
+class Person
+{
+	public int index;
+	public String Name;
+	public String Mail_Address;
+	public String Phone;
 }
