47b81d65d08549684717e7fcb1b267ddeec837db
sung_min <none@none>
2011-08-18 10:11:52 +0900

    wpf 기본 레이아웃 구축 메일 페이지&그래프 페이지 구축남음

---
+++ test/testWindow/testWindow/second.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace testWindow
+{
+    /// <summary>
+    /// Interaction logic for second.xaml
+    /// </summary>
+    public partial class second : Page
+    {
+
+        public second()
+        {
+            InitializeComponent();
+
+            Storyboard story = (Storyboard)FindResource("logInDone");
+            story.Begin();
+        }
+
+        private void mail_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Storyboard story = (Storyboard)FindResource("clearMail");
+            story.Begin();
+        }
+
+        private void mail_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Storyboard story = (Storyboard)FindResource("clearMail");
+            story.Remove();
+        }
+
+        private void chart_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            Storyboard story = (Storyboard)FindResource("clearChart");
+            story.Begin();
+        }
+
+        private void chart_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+            Storyboard story = (Storyboard)FindResource("clearChart");
+            story.Remove();
+        }
+
+        private void page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
