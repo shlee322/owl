8acef14df334ce86a8f4b6ed712bf83ceb152e74
stephan@stephan-PC <stephan@stephan-PC>
2011-08-20 05:42:00 +0900

    추가

---
+++ test/testwindowLast/testWindow/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace testWindow
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static public string id;
+        static public string passwd;
+        static public Boolean isLogined;
+        static public Window originWindow;
+    }
+}
---
+++ test/testwindowLast/testWindow/writeMail.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace testWindow
+{
+    /// <summary>
+    /// Interaction logic for writeMail.xaml
+    /// </summary>
+    public partial class writeMail : Page
+    {
+        public writeMail()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            bool dropEnabled = true; 
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true)) 
+            { 
+                string[] filenames = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+                foreach (string filename in filenames)
+                {
+                    if (System.IO.Path.GetExtension(filename).ToUpperInvariant() != ".PNG" || System.IO.Path.GetExtension(filename).ToUpperInvariant() != ".BMP")
+                    {
+                        dropEnabled = false; 
+                        break;
+                    }
+                } 
+            } else
+            { 
+                dropEnabled = false; 
+            } 
+            if (!dropEnabled) 
+            {
+                e.Effects = DragDropEffects.None; 
+                e.Handled = true;
+            }
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            string[] droppedFilenames = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+            BitmapImage sImage = new BitmapImage();
+            sImage.BeginInit();
+            sImage.UriSource = new Uri(droppedFilenames[0], UriKind.RelativeOrAbsolute);
+            sImage.EndInit();
+            senderImg.Source = sImage;
+        }
+
+        private void address_DragOver(object sender, DragEventArgs e)
+        {
+            bool dropEnabled = true;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                string[] filenames = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+            }
+            else
+            {
+                dropEnabled = false;
+            }
+            if (!dropEnabled)
+            {
+                e.Effects = DragDropEffects.None; 
+                e.Handled = true;
+            }
+        }
+
+        private void address_Drop(object sender, DragEventArgs e)
+        {
+            string[] droppedFilenames = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+            StreamReader sreader = new StreamReader(droppedFilenames[0],System.Text.Encoding.UTF32);
+            string line = string.Empty;
+
+            while( (line=sreader.ReadLine())!= null )
+            {
+                MessageBox.Show(line);
+            }
+            sreader.Close();
+        }
+    }
+}
